9e84d8ca1f76fe9d1f499229844edd1f099bf77a
Bayartsengel Baatarkhuyag <bayartsengelt@gmail.com>
2021-04-06 04:17:31 +1000

    checking the options

--- user_upload.php
+++ user_upload.php
@@ -1,4 +1,62 @@
 <?php
+
+function db_connect(){
+    $config = new stdClass();
+    $config->dbserver = '127.0.0.1';
+    $config->dbuser='root';
+    $config->dbpass='';
+    $config->dbname='test';
+
+    $connection  = mysqli_connect($config->dbserver,
+                                $config->dbuser,
+                                $config->dbpass,
+                                $config->dbname);
+
+    if (!$connection) {
+        die("Could not connect:" . mysqli_connect_error());
+      };
+     echo 'Connected successfully';
+
+    return $connection;
+ }
+
+ function db_close($connection)
+    {
+        mysqli_close($connection);
+    }
+
+    function create_table($connection)
+    {
+        $sql = "create table userlist(
+            id INT AUTO_INCREMENT, 
+            name VARCHAR(50) NOT NULL,
+            surname VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            PRIMARY KEY (`id`))";
+        if(mysqli_query($connection,$sql))
+        {
+            echo "\r\nUser table created successfully";
+        }
+        else
+        {
+            echo "\r\nCould not create table: ". mysqli_error($connection); 
+        }
+    }
+
+    function insert_data($connection,$name,$surname,$email)
+    {
+        $sql = "INSERT into userlist(name,surname,email)
+        VALUES ('$name','$surname','$email')";
+
+        if(mysqli_query($connection, $sql)){  
+            echo "\r\nRecord inserted successfully";  
+           }else{  
+            echo "\r\nCould not insert record: ". mysqli_error($connection);  
+           }  
+
+    }
+
+
 $clioption = array(
     "file:",
     "create_table::",
